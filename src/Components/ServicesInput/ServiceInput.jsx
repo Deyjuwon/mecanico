036f73a686472c2df7008ba0525a4e7b7d364c10
Adejuwon Oshindoro <temitayoadejuwon@gmail.com>
2024-01-02 23:02:55 +0100

    updated serices input

--- src/Components/ServicesInput/ServiceInput.jsx
+++ src/Components/ServicesInput/ServiceInput.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import './ServiceInput.css'
+
 
 const ServiceInput = () => {
   return (
-    <form action="">
-        <input type="text" />
+    <form className='serviceForm' action="">
+        <input className='serviceInput' type="text" placeholder='Search Services' />
+        <button type="submit" className='serviceBtn'>Get Estimate</button>
+     
     </form>
   )
 }
