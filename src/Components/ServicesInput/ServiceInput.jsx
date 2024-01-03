0712ce9af39317f4f326a9a1a48b7ad8a3e7c732
Adejuwon Oshindoro <temitayoadejuwon@gmail.com>
2024-01-03 22:09:11 +0100

    updated serices input drop domn menu

--- src/Components/ServicesInput/ServiceInput.jsx
+++ src/Components/ServicesInput/ServiceInput.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ServiceInput.css'
+import { CiSearch } from "react-icons/ci";
 
 
 const ServiceInput = () => {
+
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+
+  // Function to toggle the dropdown visibility
+  const toggleDropdown = () => {
+    setDropdownOpen(!isDropdownOpen);
+  };
   return (
     <form className='serviceForm' action="">
-        <input className='serviceInput' type="text" placeholder='Search Services' />
+        <input className='serviceInput' type="text" placeholder='Search Services' onClick={toggleDropdown} />
+
         <button type="submit" className='serviceBtn'>Get Estimate</button>
+        <CiSearch className='searchIcon' size={26} />
+
+        {isDropdownOpen && (
+        <div className='serviceDD'>
+          <ul>
+            <li>Oil Change</li>
+            <li>Brake Pads</li>
+            <li>Wheel Alignment</li>
+            <li>A/C Diagnosis</li>
+            <li>Brake Fluid</li>
+            <li>Transmission Flush</li>
+            <li>General Diagnosis</li>
+          </ul>
+        </div>
+      )}
      
     </form>
   )
