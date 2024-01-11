d2518731ce7dfac60606984ee63f489ace2205d2
Adejuwon Oshindoro <temitayoadejuwon@gmail.com>
2024-01-11 18:57:51 +0100

    updated styling of whyMecanico component

--- src/Components/WhyMecanico/WhyMecanico.jsx
+++ src/Components/WhyMecanico/WhyMecanico.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import './WhyMecanico.css'
+import { FaTools } from "react-icons/fa";
+import { RiMoneyDollarBoxFill } from "react-icons/ri";
+import { IoPricetags } from "react-icons/io5";
+import { RiGlobalFill } from "react-icons/ri";
+import { FaCalendarAlt } from "react-icons/fa";
 
 const WhyMecanico = () => {
   return (
     <div className='whyMec'>
-      <h1>Why Should I Book Service Through Mecanico</h1>
+      <h1>Why Should I Book Service Through Mecanico?</h1>
       <div>
         <ul>
-          <li>1,000s of services to select from.</li>
-          <li>Multiple competitive price estimates to compare.</li>
-          <li>Discounts up to 25% on automotive services.</li>
-          <li>Curated automotive shop network.</li>
-          <li>v</li>
+          <li><span><FaTools size={30} /></span><span>1,000s of services to select from.</span></li>
+          <li><span><RiMoneyDollarBoxFill size={30}/></span><span>Multiple competitive price estimates to compare.</span></li>
+          <li><span><IoPricetags size={30}/></span><span>Discounts up to 25% on automotive services.</span></li>
+          <li><span><RiGlobalFill size={30}/></span><span>Curated automotive shop network.</span></li>
+          <li><span><FaCalendarAlt size={30}/></span><span>Book service appointments online with confirmation.</span></li>
         </ul>
       </div>
     </div>
