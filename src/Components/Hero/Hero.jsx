ca7b47a12940e4e1e2a2de437b6e8903d49101a3
Adejuwon Oshindoro <temitayoadejuwon@gmail.com>
2024-01-11 19:26:01 +0100

    added an onscroll animation to the hero an whyMecanico section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Hero.css'
 import ServiceInput from '../ServicesInput/ServiceInput'
 import ManOnCall from '/src/assets/man-on-call.png'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 
 const Hero = () => {
+  useEffect(() => {
+      AOS.init({duration:2000})
+  }, [])
   return (
     <section className='hero '>
         <hr />
         <div className="hero-container px">
-            <div className="a-l-hero p-hero">
+            <div className="a-l-hero p-hero" data-aos="fade-up">
               <div className='l-hero'>
                 <h1>Unleashing Seamless Auto Solutions!</h1>
                 <p>Schedule your vehicle service and repair. Connect with tested and trusted professionals and technicians. Buy quality and affordable car parts.</p>
@@ -20,7 +25,7 @@ const Hero = () => {
               </div>
                 
             </div>
-            <div className="r-hero p-hero">
+            <div className="r-hero p-hero" data-aos="zoom-out">
                 <img src={ManOnCall} alt="" />
                      
             </div>
--- src/Components/WhyMecanico/WhyMecanico.jsx
+++ src/Components/WhyMecanico/WhyMecanico.jsx
@@ -1,22 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './WhyMecanico.css'
 import { FaTools } from "react-icons/fa";
 import { RiMoneyDollarBoxFill } from "react-icons/ri";
 import { IoPricetags } from "react-icons/io5";
 import { RiGlobalFill } from "react-icons/ri";
 import { FaCalendarAlt } from "react-icons/fa";
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 const WhyMecanico = () => {
+  useEffect(() => {
+      AOS.init({duration:2000})
+  }, [])
   return (
-    <div className='whyMec'>
-      <h1>Why Should I Book Service Through Mecanico?</h1>
+    <div className='whyMec' >
+      <h1 data-aos="fade-up">Why Should I Book Service Through Mecanico?</h1>
       <div>
         <ul>
-          <li><span><FaTools size={30} /></span><span>1,000s of services to select from.</span></li>
-          <li><span><RiMoneyDollarBoxFill size={30}/></span><span>Multiple competitive price estimates to compare.</span></li>
-          <li><span><IoPricetags size={30}/></span><span>Discounts up to 25% on automotive services.</span></li>
-          <li><span><RiGlobalFill size={30}/></span><span>Curated automotive shop network.</span></li>
-          <li><span><FaCalendarAlt size={30}/></span><span>Book service appointments online with confirmation.</span></li>
+          <li data-aos="fade-up"><span><FaTools size={30} /></span><span>1,000s of services to select from.</span></li>
+          <li data-aos="fade-up"><span><RiMoneyDollarBoxFill size={30}/></span><span>Multiple competitive price estimates to compare.</span></li>
+          <li data-aos="fade-up"><span><IoPricetags size={30}/></span><span>Discounts up to 25% on automotive services.</span></li>
+          <li data-aos="fade-up"><span><RiGlobalFill size={30}/></span><span>Curated automotive shop network.</span></li>
+          <li data-aos="fade-up"><span><FaCalendarAlt size={30}/></span><span>Book service appointments online with confirmation.</span></li>
         </ul>
       </div>
     </div>
