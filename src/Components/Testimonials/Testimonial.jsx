0dac339ec122f40bde9614045581c7f5bb1e6c66
Adejuwon Oshindoro <temitayoadejuwon@gmail.com>
2024-01-12 14:06:19 +0100

    made changes to the styling of the testimonial card

--- src/Components/Testimonials/Testimonial.jsx
+++ src/Components/Testimonials/Testimonial.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Testimonial.css'
 import TestimonialCard from '../TestimonialCard/TestimonialCard';
 import testimonials from '../testimonials.js'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 const Testimonial = () => {
+    useEffect(() => {
+        AOS.init({duration: 2000})
+    }, [])
+
   return (
     <div className='testimonial-container'>
-        <h1>What mecanico users are saying</h1>
-        <div>
+        <h1 data-aos='fade-up'>What mecánico users are saying</h1>
+        <div data-aos='fade-up'>
             {testimonials.map((testimonial) => 
             <TestimonialCard key={testimonial.id} name={testimonial.name}
                testimony={testimonial.testimony} />)}
