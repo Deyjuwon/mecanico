3111abde206fbcd4e4754d8377c2ce414e4e5a6c
Adejuwon Oshindoro <temitayoadejuwon@gmail.com>
2024-01-15 19:15:37 +0100

    updated and styled footer component

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import './Footer.css'
+import { FaFacebookF } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaInstagramSquare } from "react-icons/fa";
+import { CiTwitter } from 'react-icons/ci';
 
 const Footer = () => {
   return (
     <footer className='px'>
       <div className='ft ft-1'>
         <p><span>mecá</span><span>nico</span></p>
+        <div className='socials'>
+          <a href="#"><FaFacebookF size={24} /></a>
+          <a href="https://twitter.com/mecanico20000" target='_blank'><FaXTwitter size={24}/></a>
+          <a href="#"><FaInstagramSquare size={24}/></a>
+          
+        </div>
+        
       </div>
       <div className='ft ft-2'>
         <ul>
