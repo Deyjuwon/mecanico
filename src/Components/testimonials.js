cbeb090a14cc682b9aad24bb0301235949e91954
Adejuwon Oshindoro <temitayoadejuwon@gmail.com>
2024-01-18 21:56:19 +0100

    changed some styling

--- src/Components/testimonials.js
+++ src/Components/testimonials.js
@@ -3,7 +3,7 @@ const testimonials = [
         id: 1,
         name: "John Dosu",
         testimony: "mecánico is the only way I book service for my cars. It couldn't be easier: choose what services you need (or describe the problem) and you'll get 12-13 estimates from shops in the area.",
-        imgUrl: "/assets/TImg2.jpeg"
+        imgUrl: "src/assets/TImg2.jpeg"
     },
     {
         id: 2,
