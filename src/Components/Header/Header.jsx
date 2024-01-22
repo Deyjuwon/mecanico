c603d5e08135c79eeb35c1df192eb59af2932669
Adejuwon Oshindoro <temitayoadejuwon@gmail.com>
2024-01-22 06:08:22 +0100

    cleaned up header.jsx

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,19 +7,31 @@ import { Link, NavLink } from 'react-router-dom';
 const Header = () => {
 
     const [isOpened, setIsOpened] = useState(false)
+    const navItems = [
+        { to: '/Individuals', label: 'Individuals' },
+        { to: '/Professionals', label: 'Professionals' },
+        { to: '/Services', label: 'Services' },
+        { to: '/Marketplace', label: 'Marketplace' },
+      ];
+      
   
 
   return (
     <header className='px'>
         <div className="l-header">
-            <Link to="./"><p><span className='meca'>mecá</span><span>nico</span></p></Link>
+            <Link to="/"><p><span className='meca'>mecá</span><span>nico</span></p></Link>
         </div>
         <div className={isOpened ? 'mobile-nav' : 'r-header'}>
             <ul>
-                <NavLink to="./Individuals"><li>Individuals</li></NavLink>
-                <NavLink to="./Professionals"><li>Professionals</li></NavLink>
-                <NavLink to="./Services"><li>Services</li></NavLink>
-                <NavLink to="./Marketplace"><li>Marketplace</li></NavLink>
+                {/* <NavLink onClick={() => setIsOpened(!isOpened)} to="./Individuals"><li>Individuals</li></NavLink>
+                <NavLink onClick={() => setIsOpened(!isOpened)} to="./Professionals"><li>Professionals</li></NavLink>
+                <NavLink onClick={() => setIsOpened(!isOpened)} to="./Services"><li>Services</li></NavLink>
+                <NavLink onClick={() => setIsOpened(!isOpened)} to="./Marketplace"><li>Marketplace</li></NavLink> */}
+                {navItems.map((item, index) => (
+                    <NavLink key={index} to={item.to}>
+                    <li>{item.label}</li>
+                    </NavLink>
+                ))}
             </ul>
         </div>
         
